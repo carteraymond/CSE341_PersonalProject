e890a1714db48ec385d6aeda344848605d4b13a0
carteraymond <craymond78@gmail.com>
2024-06-02 01:19:03 -0400

    remade Swagger

--- swagger.js
+++ swagger.js
@@ -5,11 +5,13 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'localhost:8080'
+  host: 'cse341-personalproject-fcdb.onrender.com',
+  basePath: "/",
+  schemes: ["http"]
 };
 
 const outputFile = './swagger.json';
-const routes = ['./routes/index.js', './routes/staff.js'];
+const routes = ['./routes/index.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
