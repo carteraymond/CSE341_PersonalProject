b0cf5ecff2c80a9d11cce06ab5349253f84747a3
carteraymond <craymond78@gmail.com>
2024-06-02 01:33:17 -0400

    removed CORS

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
-const cors = require('cors');
+// const cors = require('cors');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(bodyParser.json()).use(cors()).use('/', require('./routes'));
+// app.use(bodyParser.json()).use(cors()).use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
