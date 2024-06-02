a5c2f57bcdeb07e7a9ea2962552d3c1aa1c3ac31
carteraymond <craymond78@gmail.com>
2024-06-02 00:32:29 -0400

    swagger attempt

--- swagger.js
+++ swagger.js
@@ -9,7 +9,7 @@ const doc = {
 };
 
 const outputFile = './swagger.json';
-const routes = ['./routes/index.js', './routes/staff.js'];
+const routes = ['./routes/index.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
